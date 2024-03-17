3226d43fc5a78dd70ab5dfa5b13f460d4a62f2f3
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-03-17 12:43:33 +0100

    list of courses

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import React from "react";
+
+import Button from "@/components/Button";
+import ListItem from "@/components/ListItem";
 
 function Library() {
   // New Course Button
@@ -6,7 +11,18 @@ function Library() {
   // Course Functionality (link)
   // Order => most used, alphabeticly, date created, date last used
   // Delete course (Only in the course not this page)
-  return <div>content</div>;
+  return (
+    <div className="w-full max-w-xl bg-white p-8 rounded shadow-lg mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Courses</h1>
+      <Button onClick={() => {}}>Create New Course</Button>
+      <ul className="mt-2">
+        <ListItem
+          title="Course 1"
+          descriptions={{ Sets: 3, Cards: 42 }}
+        ></ListItem>
+      </ul>
+    </div>
+  );
 }
 
 export default Library;
