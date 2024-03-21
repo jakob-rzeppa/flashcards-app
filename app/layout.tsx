6d47ae6869f4060a29761b419e966f9f03d409ed
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-03-21 21:24:00 +0100

    header placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
+import Header from "@/components/Header";
+
 export const metadata = {
   title: "flashcards-app",
   description: "",
@@ -15,6 +17,7 @@ export default function RootLayout({
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
+          <Header />
           {children}
         </main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Header from "@/components/Header";
-
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <Header />
-    </div>
+    <div className="flex-1 w-full flex flex-col gap-20 items-center"></div>
   );
 }
