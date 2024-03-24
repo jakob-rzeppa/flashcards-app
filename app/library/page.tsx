701f6913c8f3e230071efcc0fa289350af7413d2
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-03-24 07:18:07 +0100

    new Course modal

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -67,6 +67,13 @@ function LibraryPage() {
       getNumOfFolders(course.id);
     });
 
+  const [newCourseTitle, setNewCourseTitle] = useState("");
+  const [newCourseDescription, setNewCourseDescription] = useState("");
+
+  const handleCreateCourse = () => {
+    console.log(newCourseTitle);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center gap-8 mt-8">
       <div className="flex gap-4 w-full max-w-6xl justify-center flex-wrap">
@@ -79,10 +86,9 @@ function LibraryPage() {
       <div className="w-4/5 carousel space-x-4 items-center">
         {courses ? (
           courses.map((course, index) => (
-            <div className="carousel-item">
+            <div className="carousel-item" key={course.id}>
               <Course
                 data={course}
-                key={course.id}
                 numOfFolders={numOfFoldersPerCourse[index]}
               />
             </div>
@@ -90,7 +96,46 @@ function LibraryPage() {
         ) : (
           <p>Loading Courses...</p>
         )}
-        <button className="btn btn-primary">Create a new Course</button>
+        <button
+          className="btn btn-primary"
+          onClick={() =>
+            (
+              document.getElementById("create_course_modal") as HTMLFormElement
+            ).showModal()
+          }
+        >
+          Create a new Course
+        </button>
+        <dialog id="create_course_modal" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <h3 className="font-bold text-lg">Create a Course</h3>
+            <input
+              type="text"
+              className="input input-primary mt-4"
+              placeholder="title"
+              onChange={(event) => setNewCourseTitle(event.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              className="input input-primary mt-2"
+              placeholder="description"
+              onChange={(event) => setNewCourseDescription(event.target.value)}
+            />
+            <br />
+            <button
+              className="btn btn-primary mt-4"
+              onClick={handleCreateCourse}
+            >
+              Create
+            </button>
+          </div>
+        </dialog>
       </div>
     </div>
   );
