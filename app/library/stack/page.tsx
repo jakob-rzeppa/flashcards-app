7d2625cd25a20e22ea0c3a3b0c5a351a19038ed0
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-07 11:15:24 +0200

    stack actions

--- app/library/stack/page.tsx
+++ app/library/stack/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import getCards from "@/actions/getCards";
+import StackActions from "./StackActions";
 
 interface Props {
   searchParams: { id: string };
@@ -12,6 +13,7 @@ async function StackPage({ searchParams: { id } }: Props) {
   return (
     <div className="flex flex-col gap-8 justify-center items-center mt-8">
       <h1 className="text-4xl font-bold">{"stack name"}</h1>
+      <StackActions id={id} />
       <h2 className="text-3xl font-semibold">Cards</h2>
       <div className="flex flex-col w-3/5 gap-3">
         {cards?.map((card, index) => (
