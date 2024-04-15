d2b5fedce496c936f977956eb51d4fa54da99033
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-15 08:37:54 +0200

    getCourseData

---
+++ actions/getCourseData.ts
@@ -0,0 +1,29 @@
+import { createClient } from "@/utils/supabase/server";
+
+export async function getCourseData(id: number) {
+  const supabase = createClient();
+
+  const course = await supabase.from("courses").select("*").eq("id", id);
+
+  const folders = await supabase
+    .from("folders")
+    .select("*")
+    .eq("course_id", id);
+
+  if (course.error) {
+    console.error(course.error);
+    return { data: null, folders: null };
+  }
+
+  if (course.data.length === 0) {
+    console.error("CourseId not found!");
+    return { data: null, folders: null };
+  }
+
+  if (folders.error) {
+    console.error(folders.error);
+    return { data: course.data[0], folders: null };
+  }
+
+  return { data: course.data[0], cards: folders.data };
+}
--- app/(site)/library/course/page.tsx
+++ app/(site)/library/course/page.tsx
@@ -3,17 +3,19 @@ import React from "react";
 import FoldersList from "@/app/(site)/library/course/FoldersList";
 import getName from "@/actions/getName";
 import NavButton from "@/components/navigation/NavButton";
+import { getCourseData } from "@/actions/getCourseData";
 
 interface Props {
   searchParams: { id: string };
 }
 
 async function CoursePage({ searchParams: { id } }: Props) {
-  const name = await getName("courses", parseInt(id));
+  //TODO error handling
+  const data = await getCourseData(parseInt(id));
 
   return (
     <div className="flex flex-col gap-8 justify-center items-center mt-8 relative">
-      <h1 className="text-4xl font-bold">{name}</h1>
+      <h1 className="text-4xl font-bold">{data.data!.name}</h1>
       <FoldersList courseId={id} />
     </div>
   );
