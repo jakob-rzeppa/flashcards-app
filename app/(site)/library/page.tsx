bb2e58e7c1b10c0eac5096161958ed1fed788a8e
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-16 06:45:10 +0200

    remove courseList

--- app/(site)/library/page.tsx
+++ app/(site)/library/page.tsx
@@ -1,12 +1,27 @@
+import getLibraryData from "@/actions/library/getLibraryData";
+import LibraryList from "@/components/library/LibraryList";
 import React from "react";
 
-import CourseList from "@/app/(site)/library/CoursesList";
-
 async function LibraryPage() {
+  // TODO error handling
+  const courses = await getLibraryData();
+
+  const data: {
+    href: string;
+    name: string;
+    description: string | null;
+    badgeText: string;
+  }[] = courses!.data.map((course, index) => ({
+    href: "/library/course?id=" + course.id,
+    name: course.name,
+    description: course.description,
+    badgeText: "Folders: " + courses?.numOfFolders[index],
+  }));
+
   return (
     <div className="flex flex-col justify-center items-center gap-8 mt-8">
       <h2 className="text-3xl">Courses</h2>
-      <CourseList />
+      <LibraryList data={data} />
     </div>
   );
 }
