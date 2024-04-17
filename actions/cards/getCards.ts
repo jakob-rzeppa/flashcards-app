cd0408aaac638805f78e5bea4fd3ec92275f0915
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-17 19:28:33 +0200

    get card level / create new card level entry

--- actions/cards/getCards.ts
+++ actions/cards/getCards.ts
@@ -1,14 +1,45 @@
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 
 export async function getCards() {
   const supabase = createClient();
 
-  const { data, error } = await supabase.from("cards").select("*");
+  const user = await supabase.auth.getUser();
 
-  if (error) {
-    console.error(error);
+  if (user.error) {
+    console.error("No User");
     return [];
   }
 
-  return data;
+  const userId = user.data.user!.id;
+
+  const cards = await supabase.from("cards").select("*");
+
+  if (cards.error) {
+    console.error(cards.error);
+    return [];
+  }
+
+  const cardIdLevel: { cardId: number; level: number }[] = cards.data.map(
+    (card) => {
+      return { cardId: card.id, level: 0 };
+    }
+  );
+
+  for (let i = 0; i < cardIdLevel.length; i++) {
+    const level = await supabase
+      .from("card_level")
+      .select("*")
+      .eq("card_id", cards.data[i].id)
+      .eq("user_id", userId);
+
+    if (level.error || level.data.length !== 1) {
+      await supabase.from("card_level").insert([{ card_id: cards.data[i].id }]);
+      console.log("created new level entry");
+      continue;
+    }
+
+    cardIdLevel[i].level = level.data[0].level;
+  }
+
+  return cards.data;
 }
--- supabase.ts
+++ supabase.ts
@@ -9,7 +9,7 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      box_level: {
+      card_level: {
         Row: {
           card_id: number
           level: number
