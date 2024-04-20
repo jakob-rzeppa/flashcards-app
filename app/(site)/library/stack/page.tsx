f52a16013e74a98426752776b307659b65c6e963
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-20 13:07:47 +0200

    cards level display

--- app/(site)/library/stack/page.tsx
+++ app/(site)/library/stack/page.tsx
@@ -5,6 +5,7 @@ import NavButton from "@/components/navigation/NavButton";
 import { getStackData } from "@/actions/library/getStackData";
 import StackSettings from "./StackSettings";
 import BackgroundBox from "@/components/BackgroundBox";
+import { getAllCardLevels } from "@/actions/cards/getAllCardLevels";
 
 interface Props {
   searchParams: { id: string };
@@ -13,6 +14,10 @@ interface Props {
 async function StackPage({ searchParams: { id } }: Props) {
   // TODO error handling
   const { data, cards } = await getStackData(parseInt(id));
+
+  const levels = await getAllCardLevels(cards!);
+
+  console.log(levels);
 
   return (
     <div className="mt-8">
@@ -25,6 +30,20 @@ async function StackPage({ searchParams: { id } }: Props) {
         {data?.name}
       </h1>
       <BackgroundBox>
+        <div className="w-4/5 h-2 bg-red-600 rounded-full flex flex-row overflow-hidden">
+          <div
+            className={`h-full bg-green-500`}
+            style={{ width: `${(levels[3] / cards?.length!) * 100}%` }}
+          ></div>
+          <div
+            className={`h-full bg-yellow-500`}
+            style={{ width: `${(levels[2] / cards?.length!) * 100}%` }}
+          ></div>
+          <div
+            className={`h-full bg-orange-500`}
+            style={{ width: `${(levels[1] / cards?.length!) * 100}%` }}
+          ></div>
+        </div>
         <StackContent cardsData={cards ? cards : []} id={parseInt(id)} />
       </BackgroundBox>
     </div>
