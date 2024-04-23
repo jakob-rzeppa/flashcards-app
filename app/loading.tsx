9e9b4f4c7f20f85c0329f317c94544b149f30e99
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-23 13:43:44 +0200

    clear project

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Loading() {
-  return <div className="loading loading-spinner loading-md"></div>;
-}
-
-export default Loading;
--- components/navigation/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <nav className="h-min w-full border-t border-t-foreground/10 static bottom-0">
-      <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm mx-auto">
-        Footer
-      </div>
-    </nav>
-  );
-}
