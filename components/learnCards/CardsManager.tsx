e6929d4fd4107d28a8fc7875991f34ab2313a78c
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-04-27 06:52:36 +0200

    progress display between each learn session

--- components/learnCards/CardsManager.tsx
+++ components/learnCards/CardsManager.tsx
@@ -8,6 +8,7 @@ import { getCardsToUse } from "@/actions/cards/client/getCardsToUse";
 
 import Cards from "./Cards";
 import BrowserCards from "./BrowserCards";
+import { getAllCardLevels } from "@/actions/cards/client/getAllCardLevels";
 
 interface CardData {
   created_at: string;
@@ -24,15 +25,22 @@ interface Props {
 
 // Handles getting the current Cards and when all Cards are learned
 function CardsManager({ cards }: Props) {
+  const [cardLevels, setCardLevels] = useState<{
+    0: number;
+    1: number;
+    2: number;
+    3: number;
+  }>({ 0: 0, 1: 0, 2: 0, 3: 0 });
+
   const router = useRouter();
   const [level, setLevel] = useState(0);
   const [currentCards, setCurrentCards] = useState<CardData[]>([]);
   const [isActive, setIsActive] = useState(false);
   const [finished, setFinished] = useState(false);
 
-  const handleGetCards = () => {
+  const handleGetCards = (currentLevel: number) => {
     setIsActive(false);
-    const res = getCardsToUse(cards, level);
+    const res = getCardsToUse(cards, currentLevel);
 
     res.then((data: CardData[]) => {
       if (data.length <= 0) {
@@ -46,11 +54,14 @@ function CardsManager({ cards }: Props) {
   };
 
   useEffect(() => {
-    handleGetCards();
+    handleGetCards(level);
   }, [level]);
 
-  const onFinished = () => {
+  const onFinished = async () => {
     setFinished(true);
+    setIsActive(false);
+    setCardLevels(await getAllCardLevels(cards));
+    setIsActive(true);
   };
 
   const onNextLevel = () => {
@@ -60,20 +71,40 @@ function CardsManager({ cards }: Props) {
       return;
     }
     console.log("Next Box Level: ", nextLevel);
+
     setLevel(nextLevel);
     setFinished(false);
   };
 
   const onLowestLevel = () => {
-    console.log("Next Box Level: ", 0);
-    setLevel(0);
+    const nextLevel = 0;
+    console.log("Next Box Level: ", nextLevel);
+
+    setLevel(nextLevel);
     setFinished(false);
   };
+
+  useEffect(() => {
+    handleGetCards(0);
+  }, []);
 
   return isActive ? (
     finished ? (
       <div className="items-center justify-center flex flex-col h-screen gap-8">
-        <h2>Done (Level {level})</h2>
+        <div className="w-4/5 h-2 bg-red-600 rounded-full flex flex-row overflow-hidden">
+          <div
+            className={`h-full bg-green-500`}
+            style={{ width: `${(cardLevels[3] / cards?.length!) * 100}%` }}
+          ></div>
+          <div
+            className={`h-full bg-yellow-500`}
+            style={{ width: `${(cardLevels[2] / cards?.length!) * 100}%` }}
+          ></div>
+          <div
+            className={`h-full bg-orange-500`}
+            style={{ width: `${(cardLevels[1] / cards?.length!) * 100}%` }}
+          ></div>
+        </div>
         <button className="btn btn-primary" onClick={onNextLevel}>
           Next Level
         </button>
