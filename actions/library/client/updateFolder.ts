1aba22364e703172702a69bb1b9eaf4b4099760e
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-06-07 11:01:04 +0200

    updateFolder action

---
+++ actions/library/client/updateFolder.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@/utils/supabase/client";
+
+export default async function updateFolder(id: number, data: { name: string }) {
+  const supabase = createClient();
+
+  const res = await supabase.from("folders").update(data).eq("id", id);
+
+  if (res.error) {
+    console.error(res.error);
+    return false;
+  }
+
+  console.log("Updated Folder", res.data);
+  return true;
+}
